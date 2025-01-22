52d0d6321ff282d7005ff4758a50b8a4991d3423
Badr Aliloui <tranquil2021@hotmail.com>
2025-01-22 20:09:39 +0000

    add secret key in .env and come back to the beginning

--- store/urls.py
+++ store/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     path('product/<product_id>/update/', views.update_product, name='update_product'),
     path('product/<product_id>/delete/', views.delete_product, name='delete_product'),
     path('product/<product_id>/', views.product_detail, name='product_detail'),
-    path('<str:slug>/process_payment/', views.process_payment, name='process_payment'),
-    path('<str:reference>/success/', views.payment_success, name='success'),
-    path('<str:reference>/cancel/', views.payment_cancel, name='cancel'),
-    path('stripe_webhook/', views.stripe_webhook, name='stripe_webhook'),
+    # path('<str:slug>/process_payment/', views.process_payment, name='process_payment'),
+    # path('<str:reference>/success/', views.payment_success, name='success'),
+    # path('<str:reference>/cancel/', views.payment_cancel, name='cancel'),
+    # path('stripe_webhook/', views.stripe_webhook, name='stripe_webhook'),
 ]
 
 
