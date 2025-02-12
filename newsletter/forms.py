4d610d84e0b6a1a2783f5c4e122fa61735dca5e1
Badr Aliloui <tranquil2021@hotmail.com>
2025-02-12 21:10:38 +0000

    Improved the code python

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from .models import NewsletterSubscriber
 
+
 class NewsletterForm(forms.ModelForm):
     class Meta:
         model = NewsletterSubscriber
         fields = ['email']
         widgets = {
-            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'})
+            'email': forms.EmailInput(attrs={'class': 'form-control',
+                                             'placeholder': 'Your email'})
         }
